e5c0532d05e47a78227ea484a4ab05ce80ca4661
ktxdev <sean.ktxdev@gmail.com>
2022-11-11 23:34:27 +0200

    Feat: Implemented reminders functionality

--- src/main/java/com/ktxdev/allinone/internal/service/BaseService.java
+++ src/main/java/com/ktxdev/allinone/internal/service/BaseService.java
@@ -1,7 +1,15 @@
 package com.ktxdev.allinone.internal.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
 public interface BaseService<D, C, U> extends BaseReaderService<D> {
     D create(C c);
     D update(U u);
     void delete(Long id);
+    Optional<D> getById(Long id);
+    D findById(Long id);
+    Page<D> findAll(String query, Pageable pageable);
 }
--- src/main/java/com/ktxdev/allinone/internal/service/impl/BaseServiceImpl.java
+++ src/main/java/com/ktxdev/allinone/internal/service/impl/BaseServiceImpl.java
@@ -1,15 +1,32 @@
 package com.ktxdev.allinone.internal.service.impl;
 
+import com.ktxdev.allinone.internal.exceptions.RecordNotFoundException;
 import com.ktxdev.allinone.internal.repository.BaseRepository;
 import com.ktxdev.allinone.internal.service.BaseService;
-import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
-@Slf4j
-public abstract class BaseServiceImpl<D, C, U> extends BaseReaderServiceImpl<D> implements BaseService<D, C, U> {
-    public BaseServiceImpl(BaseRepository<D> repository) {
-        super(repository);
+@RequiredArgsConstructor
+public abstract class BaseServiceImpl<D, C, U> implements BaseService<D, C, U> {
+    private final BaseRepository<D> repository;
+
+    @Override
+    public Optional<D> getById(Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public D findById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RecordNotFoundException("Record not found"));
+    }
+
+    @Override
+    public Page<D> findAll(String query, Pageable pageable) {
+        return repository.findAll(pageable);
     }
 
     @Override
